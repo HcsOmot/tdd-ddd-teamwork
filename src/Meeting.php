90e291b27d0b23c9437f1097f1102f0eeca3269f
Tomo Šala <tomo.sala@undabot.com>
2019-12-06 09:09:26 +0100

    exercise 2: Meeting cannot end before it started

--- src/Meeting.php
+++ src/Meeting.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 namespace Procurios\Meeting;
 
 use DateTimeImmutable;
+use DomainException;
 use Ramsey\Uuid\UuidInterface;
 
 final class Meeting
@@ -32,8 +33,16 @@ final class Meeting
         $this->meetingId = $meetingId;
         $this->title = $title;
         $this->description = $description;
+        $this->cannotEndBeforeItStarts($start, $end);
         $this->start = $start;
         $this->end = $end;
         $this->program = $program;
+    }
+
+    private function cannotEndBeforeItStarts(DateTimeImmutable $start, DateTimeImmutable $end)
+    {
+        if ($end < $start) {
+            throw new DomainException('Meeting cannot end before it has started');
+        }
     }
 }
--- tests/MeetingTest.php
+++ tests/MeetingTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 namespace Procurios\Meeting\Tests;
 
 use DateTimeImmutable;
+use DomainException;
 use InvalidArgumentException;
 use Procurios\Meeting\Meeting;
 use Procurios\Meeting\Program;
@@ -46,5 +47,33 @@ final class MeetingTest extends TestCase
     {
         $this->expectException(InvalidArgumentException::class);
         new Program([new DateTimeImmutable('2020-01-01 19:00')]);
+    }
+
+    public function testThatMeetingCannotEndBeforeItStarted()
+    {
+        $this->expectException(DomainException::class);
+        $this->expectExceptionMessage('Meeting cannot end before it has started');
+
+        new Meeting(
+            Uuid::uuid4(),
+            new Title('TDD, DDD & Teamwork'),
+            'This is a silly workshop, don\'t come',
+            new DateTimeImmutable('2020-01-01 21:00'),
+            new DateTimeImmutable('2020-01-01 19:00'),
+            new Program([
+                new ProgramSlot(
+                    new DateTimeImmutable('2020-01-01 19:00'),
+                    new DateTimeImmutable('2020-01-01 20:00'),
+                    'Divergence',
+                    'Main room'
+                ),
+                new ProgramSlot(
+                    new DateTimeImmutable('2020-01-01 20:00'),
+                    new DateTimeImmutable('2020-01-01 21:00'),
+                    'Convergence',
+                    'Main room'
+                ),
+            ])
+        );
     }
 }
